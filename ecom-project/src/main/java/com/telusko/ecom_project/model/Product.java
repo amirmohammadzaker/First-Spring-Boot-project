e8e138f7d47cabde3b81570abb98ea9183e4c695
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-08-01 13:20:00 +0330

    feat(product): format release date in API responses

--- ecom-project/src/main/java/com/telusko/ecom_project/model/Product.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/model/Product.java
@@ -1,5 +1,6 @@
 package com.telusko.ecom_project.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,6 +27,7 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
     private boolean available;
     private int quantity;
