c654bccb897e38fe606ed0a5058431ada956a122
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-08-01 16:01:59 +0330

    feat(product): add product creation with image upload support

--- ecom-project/src/main/java/com/telusko/ecom_project/model/Product.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/model/Product.java
@@ -18,7 +18,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
     private String desc;
     private String brand;
