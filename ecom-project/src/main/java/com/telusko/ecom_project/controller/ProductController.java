5ea410f44cadf8c1feb5e1ab5270fe7645208113
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-08-01 11:24:26 +0330

    feat(product): add product retrieval API

--- ecom-project/src/main/java/com/telusko/ecom_project/controller/ProductController.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/controller/ProductController.java
@@ -1,14 +1,29 @@
 package com.telusko.ecom_project.controller;
 
 
+import com.telusko.ecom_project.model.Product;
+import com.telusko.ecom_project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
     @RequestMapping("/")
     public String greet(){
         return "Hello world";
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return service.getAllProducts();
     }
 }
