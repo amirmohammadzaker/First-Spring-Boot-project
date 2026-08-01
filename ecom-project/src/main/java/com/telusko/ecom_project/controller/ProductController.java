a0d58209ea56f3736940962057497d33985e3d4a
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-08-01 13:11:22 +0330

    feat(product): add endpoint to get product by ID

--- ecom-project/src/main/java/com/telusko/ecom_project/controller/ProductController.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/controller/ProductController.java
@@ -4,10 +4,7 @@ package com.telusko.ecom_project.controller;
 import com.telusko.ecom_project.model.Product;
 import com.telusko.ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,10 @@ public class ProductController {
     @GetMapping("/products")
     public List<Product> getAllProducts(){
         return service.getAllProducts();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product getProduct(@PathVariable int id){
+        return service.getProductById(id);
     }
 }
