ef7773f0b557a9df61c566eabcbceac8367555d5
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-08-01 13:48:56 +0330

    fix(product): handle missing products with proper HTTP responses

--- ecom-project/src/main/java/com/telusko/ecom_project/controller/ProductController.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/controller/ProductController.java
@@ -4,6 +4,9 @@ package com.telusko.ecom_project.controller;
 import com.telusko.ecom_project.model.Product;
 import com.telusko.ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,12 +25,21 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
     @GetMapping("/product/{id}")
-    public Product getProduct(@PathVariable int id){
-        return service.getProductById(id);
+    public ResponseEntity <Product> getProduct(@PathVariable int id){
+
+        Product product = service.getProductById(id);
+
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
