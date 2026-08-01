fb8ddc96f8e0645733eae8c21255e37ff2784bbc
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-08-01 16:48:58 +0330

    feat(product): add endpoint to retrieve product images

--- ecom-project/src/main/java/com/telusko/ecom_project/controller/ProductController.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.telusko.ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -53,4 +54,11 @@ public class ProductController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+   @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+        Product product = service.getProductById(productId);
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType())).body(imageFile);
+   }
 }
