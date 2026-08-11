9a7858bb97969f427a24079f0a7df24e3762ce77
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-08-11 17:13:56 +0330

    refactor(payment): use @Primary for default PaymentGateway bean

--- ecom-project/src/main/java/com/telusko/ecom_project/service/CheckoutService.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/service/CheckoutService.java
@@ -13,7 +13,6 @@ public class CheckoutService {
 
     private final ProductRepo productRepo;
 
-    @Qualifier("samanGateway")
     private final PaymentGateway paymentGateway;
 
     @Transactional
--- ecom-project/src/main/java/com/telusko/ecom_project/service/SamanGateway.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/service/SamanGateway.java
@@ -1,8 +1,10 @@
 package com.telusko.ecom_project.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class SamanGateway implements PaymentGateway{
     @Override
     public boolean processPayment(double amount) {
