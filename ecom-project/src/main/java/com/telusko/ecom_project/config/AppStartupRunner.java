9cf5a45c2cc135e3f39698e804925cdcf247458f
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-08-18 11:05:16 +0330

    refactor: switch AppStartupRunner from CommandLineRunner to ApplicationRunner

--- ecom-project/src/main/java/com/telusko/ecom_project/config/AppStartupRunner.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/config/AppStartupRunner.java
@@ -1,23 +1,28 @@
 package com.telusko.ecom_project.config;
 
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 @Component
-public class AppStartupRunner implements CommandLineRunner {
+public class AppStartupRunner implements ApplicationRunner {
 
     @Override
-    public void run(String... args) throws Exception {
-        System.out.println("=== Application Started Successfully ===");
-        System.out.println("Command-line arguments count: " + args.length);
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("=== Application Started with ApplicationRunner ===");
 
-        if (args.length > 0) {
-            System.out.println("Command-line arguments list:");
-            Arrays.stream(args).forEach(arg -> System.out.println("- " + arg));
-        } else {
-            System.out.println("No command-line arguments were provided.");
+        List<String> nonOptionArgs = args.getNonOptionArgs();
+        System.out.println("\n[Non-Option Arguments]: " + nonOptionArgs.size());
+        nonOptionArgs.forEach(arg -> System.out.println("- " + arg));
+
+        Set<String> optionNames = args.getOptionNames();
+        System.out.println("\n[Option Arguments]: " + optionNames.size());
+        for (String name : optionNames) {
+            List<String> values = args.getOptionValues(name);
+            System.out.println("- Key: " + name + " | Value(s): " + values);
         }
     }
 }
