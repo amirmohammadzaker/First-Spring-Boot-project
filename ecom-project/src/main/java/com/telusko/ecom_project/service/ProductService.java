5109a81b1dad05d421fb61c6b6938b1bd50160c4
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-08-23 17:11:40 +0330

    پیام کامیت (Git Commit Message) برای این تغییرات:  refactor: refactor ProductService using lombok and clean up constructor injection

--- ecom-project/src/main/java/com/telusko/ecom_project/service/ProductService.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/service/ProductService.java
@@ -2,23 +2,18 @@ package com.telusko.ecom_project.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.telusko.ecom_project.repo.CommonProductRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 
+@RequiredArgsConstructor
 public abstract class ProductService<T> {
 
     protected final CommonProductRepo<T> repo;
-
-    @Autowired
-    protected ObjectMapper objectMapper;
-
-    public ProductService(CommonProductRepo<T> repo) {
-        this.repo = repo;
-    }
+    protected final ObjectMapper objectMapper;
 
     public List<T> getAllProducts() {
         return repo.findAll();
@@ -28,14 +23,15 @@ public abstract class ProductService<T> {
         return repo.findById(id).orElse(null);
     }
 
-    public void deleteProduct(int id) {
-        repo.deleteById(id);
-    }
-
     public abstract T addProduct(String productJson, MultipartFile imageFile) throws IOException;
 
     public abstract T updateProduct(int id, String productJson, MultipartFile imageFile) throws IOException;
 
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
+    }
+
     public abstract List<T> searchProducts(String keyword);
+
     public abstract T updateProductPrice(int id, BigDecimal newPrice);
 }
