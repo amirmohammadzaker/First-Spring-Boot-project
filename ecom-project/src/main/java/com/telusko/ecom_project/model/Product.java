be3a89949625413790f8595f58e3d525c1411ab0
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-08-24 10:58:07 +0330

    refactor: map releaseDate field to snake_case using @JsonProperty

--- ecom-project/src/main/java/com/telusko/ecom_project/model/Product.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/model/Product.java
@@ -1,5 +1,6 @@
 package com.telusko.ecom_project.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.telusko.ecom_project.validation.ProdChecks;
 import com.telusko.ecom_project.validation.ValidReleaseDate;
 import jakarta.persistence.*;
@@ -40,6 +41,7 @@ public class Product {
 
     @NotNull(message = "تاریخ انتشار الزامی است")
     @ValidReleaseDate
+    @JsonProperty("release_Date")
     private Date releaseDate;
 
     private boolean productAvailable;
