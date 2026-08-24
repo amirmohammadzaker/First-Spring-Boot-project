7a2ad5c5f946fa63616f0d4286ed3cb4ed0a48a0
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-08-24 11:21:00 +0330

    feat: prevent password exposure in API responses using JsonProperty WRITE_ONLY

--- ecom-project/src/main/java/com/telusko/ecom_project/model/Users.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/model/Users.java
@@ -1,5 +1,6 @@
 package com.telusko.ecom_project.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,5 +18,6 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 }
